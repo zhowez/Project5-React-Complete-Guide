2261dae8bb6a30cbdfe522207633c3c4487d4978
Zach Howes <zhowes14@icloud.com>
2022-06-24 18:02:39 -0400

    began work on adding items to cart in the cartAContext

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -1,14 +1,21 @@
 import styles from "./HeaderCardButton.module.css";
 import CartIcon from "../Cart/CartIcon";
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 
 const HeaderCardButton = (props) => {
+  const cartCtx = useContext(CartContext);
+  const numberOfCartItems = cartCtx.items.reduce((curNum, item) => {
+    return curNum + item.amount;
+  }, 0);
+
   return (
     <button className={styles.button} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={styles.badge}>3</span>
+      <span className={styles.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
