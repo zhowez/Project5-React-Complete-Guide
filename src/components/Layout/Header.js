85d779b6bfb4aba49ab624b66a66eb478c59711c
Zach Howes <zhowes14@icloud.com>
2022-06-24 01:14:22 -0400

    added a header

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import styles from "./Header.module.css";
+import React from "react";
+import mealsImage from "../../assets/meals.jpg";
+import HeaderCardButton from "./HeaderCardButton";
+
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      <header className={styles.header}>
+        <h1> ReactMeals</h1>
+        <HeaderCardButton>Cart</HeaderCardButton>
+      </header>
+      <div className={styles["main-image"]}>
+        <img src={mealsImage} alt="A table full of delicious food!" />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
